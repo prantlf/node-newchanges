700db038a20881e515077638ab37297faf4a5dc7
Ferdinand Prantl <prantlf@gmail.com>
2023-10-26 23:57:23 +0200

    fix: Try retargetting the symlink on the first run

--- bin/newchanges.js
+++ bin/newchanges.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import debug from 'debug'
+// import debug from 'debug'
 import { access, symlink, unlink } from 'fs/promises'
 import { platform } from 'os'
 import { dirname, join } from 'path'
@@ -9,24 +9,29 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const exists = file => access(file).then(() => true, () => false)
-const log = debug('newchanges')
+// const log = debug('newchanges')
 
 try {
   // installed locally
-  let pkg = join(__dirname, '..', 'node-newchanges')
+  let bin = join(__dirname, '..', '..', '.bin')
+  console.log('bin "%s"', bin)
   // installed globally
-  if (!await exists(pkg)) pkg = join(__dirname, '..', 'lib', 'node_modules', 'node-newchanges')
-  if (await exists(pkg)) {
-    const exe = join(pkg, platform() != 'win32' ? 'newchanges' : `newchanges.exe`)
-    log('exe "%s"', exe)
-    if (!await exists(exe)) throw new Error('missing executable')
-    const link = join(__dirname, 'newchanges')
-    log('unlink "%s"', link)
-    await unlink(link)
-    log('link "%s"', link)
-    await symlink(exe, link, 'junction')
-    console.log('installation finished, re-run the same command')
-  }
+  if (!await exists(bin)) bin = join(__dirname, '..', '..', '..', '..', 'bin')
+  console.log('bin "%s"', bin)
+  // installed dependencies of this package
+  if (!await exists(bin)) bin = join(__dirname, '..', 'node_modules', '.bin')
+  console.log('bin "%s"', bin)
+  if (!await exists(bin)) throw new Error('cannot find bin directory')
+
+  const exe = join(__dirname, '..', platform() != 'win32' ? 'newchanges' : `newchanges.exe`)
+  console.log('exe "%s"', exe)
+  if (!await exists(exe)) throw new Error('missing executable')
+  const link = join(bin, 'newchanges')
+  console.log('unlink "%s"', link)
+  await unlink(link)
+  console.log('link "%s"', link)
+  await symlink(exe, link, 'junction')
+  console.log('installation finished, re-run the same command')
 } catch (err) {
   console.error(err)
   process.exitCode = 1
