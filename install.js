56710cf1fecb0b8bd4b02be898673f02796d8ad5
Ferdinand Prantl <prantlf@gmail.com>
2023-12-12 13:05:32 +0100

    fix: Upgrade dependencies
    BREAKING CHANGE: Instead of symlinks, wrappers .cmd and .ps1
    are created in .bin on Windows. Although it should not affect
    any scenario, it is a change in the file names nevertheless.

--- install.js
+++ install.js
@@ -5,21 +5,13 @@ import { installLink } from 'link-bin-executable'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const name = 'newchanges'
-const repository = 'prantlf/v-newchanges'
-const platformSuffixes = {
-  linux: 'linux',
-  darwin: 'macos',
-  win32: 'windows'
-}
-
 try {
   const { executable, version } = await grab({
-    repository, platformSuffixes, targetDirectory: __dirname, unpackExecutable: true
+    repository: 'prantlf/v-newchanges', targetDirectory: __dirname, unpackExecutable: true
   })
   console.log('downloaded and unpacked "%s" version %s', executable, version)
 
-  await installLink({ name, packageDirectory: __dirname })
+  await installLink({ name: 'newchanges', packageDirectory: __dirname })
 } catch (err) {
   console.error(err)
   process.exitCode = 1
